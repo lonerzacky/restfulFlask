6f85db2504db51fad29a8e961e74f86c2644588b
zackwolf <azhar.zakiy@gmail.com>
2019-01-05 16:43:40 +0700

    Cleaning code , remove too many blank line , convert lowercase function name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from dotenv import load_dotenv
 import utility
 from connection import *
@@ -10,28 +10,34 @@ app.config['JSON_SORT_KEYS'] = False
 
 
 @app.route('/', methods=['GET'])
-def getVersion():
+def get_version():
     return utility.give_response("00", os.getenv('APP_NAME'))
 
 
 @app.route('/getIdentity', methods=['GET'])
-def getIdentity():
-    cur = mysql.connect().cursor()
-    cur.execute('''select * from identity ''')
-    result = [dict((cur.description[i][0], value)
-                   for i, value in enumerate(row)) for row in cur.fetchall()]
-    return utility.give_response("00", "GET IDENTITY SUKSES", result)
+def get_identity():
+    try:
+        cur = mysql.connect().cursor()
+        cur.execute('''select * from identity ''')
+        result = [dict((cur.description[i][0], value)
+                       for i, value in enumerate(row)) for row in cur.fetchall()]
+        return utility.give_response("00", "GET IDENTITY SUKSES", result)
+    except Exception as e:
+        return utility.give_response("01", e)
 
 
 @app.route('/insertIdentity', methods=['POST'])
-def insertIdentity():
-    name = request.form["name"]
-    address = request.form["address"]
-    con = mysql.connect()
-    cur = con.cursor()
-    cur.execute("""INSERT INTO identity (name,address) VALUES (%s,%s)""", (name, address))
-    con.commit()
-    return utility.give_response("00", "INSERT IDENTITY SUKSES")
+def insert_identity():
+    try:
+        name = request.form["name"]
+        address = request.form["address"]
+        con = mysql.connect()
+        cur = con.cursor()
+        cur.execute("""INSERT INTO identity (name,address) VALUES (%s,%s)""", (name, address))
+        con.commit()
+        return utility.give_response("00", "INSERT IDENTITY SUKSES")
+    except Exception as e:
+        return utility.give_response("01", e)
 
 
 if __name__ == '__main__':
