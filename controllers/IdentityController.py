b2b516ee9f0b8e0dce9754cb001bb7154b0a02a0
zackwolf <azhar.zakiy@gmail.com>
2019-01-05 21:26:31 +0700

    Add Controller Identity

---
+++ controllers/IdentityController.py
@@ -0,0 +1,31 @@
+from flask import request
+from flask_restful import Resource
+from connection import connection
+import utility
+
+
+class GetIdentity(Resource):
+    @staticmethod
+    def get():
+        try:
+            cursor = connection.cursor()
+            cursor.execute('''select * from identity ''')
+            result = [dict((cursor.description[i][0], value)
+                           for i, value in enumerate(row)) for row in cursor.fetchall()]
+            return utility.give_response("00", "GET IDENTITY SUKSES", result)
+        except Exception as e:
+            return utility.give_response("01", e)
+
+
+class InsertIdentity(Resource):
+    @staticmethod
+    def post():
+        try:
+            cursor = connection.cursor()
+            name = request.form["name"]
+            address = request.form["address"]
+            cursor.execute("""INSERT INTO identity (name,address) VALUES (%s,%s)""", (name, address))
+            connection.commit()
+            return utility.give_response("00", "INSERT IDENTITY SUKSES")
+        except Exception as e:
+            return utility.give_response("01", e)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
+from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api, Resource
-from connection import connection
+from controllers.IdentityController import GetIdentity, InsertIdentity
 import utility
 import os
 
@@ -16,33 +16,6 @@ class GetVersion(Resource):
     @staticmethod
     def get():
         return utility.give_response("00", os.getenv('APP_NAME'))
-
-
-class GetIdentity(Resource):
-    @staticmethod
-    def get():
-        try:
-            cursor = connection.cursor()
-            cursor.execute('''select * from identity ''')
-            result = [dict((cursor.description[i][0], value)
-                           for i, value in enumerate(row)) for row in cursor.fetchall()]
-            return utility.give_response("00", "GET IDENTITY SUKSES", result)
-        except Exception as e:
-            return utility.give_response("01", e)
-
-
-class InsertIdentity(Resource):
-    @staticmethod
-    def post():
-        try:
-            cursor = connection.cursor()
-            name = request.form["name"]
-            address = request.form["address"]
-            cursor.execute("""INSERT INTO identity (name,address) VALUES (%s,%s)""", (name, address))
-            connection.commit()
-            return utility.give_response("00", "INSERT IDENTITY SUKSES")
-        except Exception as e:
-            return utility.give_response("01", e)
 
 
 api.add_resource(GetVersion, '/')
