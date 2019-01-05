8ca241f1c0f2a8dccc76aca8ffb58dd1eb58cf8e
zackwolf <azhar.zakiy@gmail.com>
2019-01-05 17:54:59 +0700

    Implementation Flask-Restful

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
 from flask import Flask, request
 from dotenv import load_dotenv
-import utility
+from flask_restful import Api, Resource
 from connection import *
+import utility
 
 load_dotenv()
 
 app = Flask(__name__)
+api = Api(app)
 app.config['JSON_SORT_KEYS'] = False
 
 
-@app.route('/', methods=['GET'])
-def get_version():
-    return utility.give_response("00", os.getenv('APP_NAME'))
+class GetVersion(Resource):
+    def get(self):
+        return utility.give_response("00", os.getenv('APP_NAME'))
 
 
-@app.route('/getIdentity', methods=['GET'])
-def get_identity():
-    try:
-        cur = mysql.connect().cursor()
-        cur.execute('''select * from identity ''')
-        result = [dict((cur.description[i][0], value)
-                       for i, value in enumerate(row)) for row in cur.fetchall()]
-        return utility.give_response("00", "GET IDENTITY SUKSES", result)
-    except Exception as e:
-        return utility.give_response("01", e)
+class GetIdentity(Resource):
+    def get(self):
+        try:
+            cur = mysql.connect().cursor()
+            cur.execute('''select * from identity ''')
+            result = [dict((cur.description[i][0], value)
+                           for i, value in enumerate(row)) for row in cur.fetchall()]
+            return utility.give_response("00", "GET IDENTITY SUKSES", result)
+        except Exception as e:
+            return utility.give_response("01", e)
 
 
-@app.route('/insertIdentity', methods=['POST'])
-def insert_identity():
-    try:
-        name = request.form["name"]
-        address = request.form["address"]
-        con = mysql.connect()
-        cur = con.cursor()
-        cur.execute("""INSERT INTO identity (name,address) VALUES (%s,%s)""", (name, address))
-        con.commit()
-        return utility.give_response("00", "INSERT IDENTITY SUKSES")
-    except Exception as e:
-        return utility.give_response("01", e)
+class InsertIdentity(Resource):
+    def post(self):
+        try:
+            name = request.form["name"]
+            address = request.form["address"]
+            con = mysql.connect()
+            cur = con.cursor()
+            cur.execute("""INSERT INTO identity (name,address) VALUES (%s,%s)""", (name, address))
+            con.commit()
+            return utility.give_response("00", "INSERT IDENTITY SUKSES")
+        except Exception as e:
+            return utility.give_response("01", e)
+
+
+api.add_resource(GetVersion, '/')
+api.add_resource(GetIdentity, '/getIdentity')
+api.add_resource(InsertIdentity, '/insertIdentity')
 
 
 if __name__ == '__main__':
