f9a5332e04799813722f540344f377226926c7db
zackwolf <azhar.zakiy@gmail.com>
2019-01-05 15:36:53 +0700

    Import MySql Connection from main

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 from flask import Flask, jsonify
-from flaskext.mysql import MySQL
-import os
 from dotenv import load_dotenv
-
 import utility
+from connection import *
 
 load_dotenv()
 
 app = Flask(__name__)
-mysql = MySQL()
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = os.getenv('DB_USERNAME')
-app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('DB_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] = os.getenv('DB_DATABASE')
-app.config['MYSQL_DATABASE_HOST'] = os.getenv('APP_HOST')
 app.config['JSON_SORT_KEYS'] = False
-mysql.init_app(app)
 
 
 @app.route('/', methods=['GET'])
