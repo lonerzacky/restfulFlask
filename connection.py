a0710457fda9ba938a01a03aae2796215580293d
zackwolf <azhar.zakiy@gmail.com>
2019-01-05 21:23:34 +0700

    Replace flask-mysql with Pmysql

--- connection.py
+++ connection.py
@@ -1,13 +1,13 @@
-from flaskext.mysql import MySQL
 import os
+import pymysql.cursors
+from dotenv import load_dotenv
 
-from main import app
+load_dotenv()
 
-mysql = MySQL()
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = os.getenv('DB_USERNAME')
-app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('DB_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] = os.getenv('DB_DATABASE')
-app.config['MYSQL_DATABASE_HOST'] = os.getenv('APP_HOST')
-mysql.init_app(app)
+config = {
+    'user'    : os.getenv('DB_USERNAME'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host'    : os.getenv('APP_HOST'),
+    'database': os.getenv('DB_DATABASE')
+}
+connection = pymysql.connect(**config)
