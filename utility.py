b85faf0e22330005f6c8f1f2e3b8950c51bc271f
zackwolf <azhar.zakiy@gmail.com>
2019-01-07 11:15:15 +0700

    Add CreateHash Function (SHA1)

--- utility.py
+++ utility.py
@@ -1,9 +1,12 @@
+import hashlib
+
 from flask import jsonify
+
 
 def give_response(response_code, response_message, response_data=""):
     return jsonify(
-        {
-            'response_code': response_code,
-            'response_message': response_message,
-            'response_data': response_data
-        })
+        {'response_code': response_code, 'response_message': response_message, 'response_data': response_data})
+
+
+def create_hash(var):
+    return hashlib.sha1(var.encode()).hexdigest()
