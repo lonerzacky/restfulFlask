0f502b8ea2cb29c3dd5ba5756eb8d560e8b4390f
zackwolf <azhar.zakiy@gmail.com>
2019-01-18 17:26:33 +0800

    Create Logging Rest API to table MySql

--- queryUtils.py
+++ queryUtils.py
@@ -1,5 +1,7 @@
+import datetime
+import json
 from typing import Dict, List, Any
-
+from flask import request
 from connection import connection
 
 
@@ -21,3 +23,18 @@ def get_info_user(sysuser_nama, sysuser_passw):
     result: List[Dict[Any, Any]] = [dict((cursor.description[i][0], value)
                                          for i, value in enumerate(row)) for row in cursor.fetchall()]
     return result
+
+
+# noinspection SqlResolve
+def create_log(response,status):
+    cursor = connection.cursor()
+    now = datetime.datetime.now()
+    request_time = now.strftime("%Y-%m-%d %H:%M")
+    if not request.form:
+        request_form = json.dumps("none")
+    else:
+        request_form = json.dumps(request.form)
+    cursor.execute(
+        """INSERT INTO logservice (uri,method,params,ip_address,request_time,response,status) VALUES (%s,%s,%s,%s,%s,%s,%s)""",
+        (request.base_url, request.method, request_form, request.remote_addr, request_time, response,status))
+    connection.commit()
--- utility.py
+++ utility.py
@@ -1,11 +1,16 @@
-import hashlib
+import hashlib, queryUtils, json
 
 from flask import jsonify
 
 
 def give_response(response_code, response_message, response_data=""):
-    return jsonify(
-        {'response_code': response_code, 'response_message': response_message, 'response_data': response_data})
+    if response_code == "00":
+        status = "success"
+    else:
+        status = "failed"
+    response = {'response_code': response_code, 'response_message': response_message, 'response_data': response_data}
+    queryUtils.create_log(json.dumps(response), status)
+    return jsonify(response)
 
 
 def create_hash(var):
