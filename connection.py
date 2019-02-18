2a7c098904356a23f107d9491f8de169d4d0203d
zackwolf <azhar.zakiy@gmail.com>
2019-02-18 16:31:37 +0700

    Add Config Unix Socket

--- connection.py
+++ connection.py
@@ -4,12 +4,13 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
-load_dotenv()
+load_dotenv(dotenv_path)
 
 config = {
     'user': os.getenv('DB_USERNAME'),
     'password': os.getenv('DB_PASSWORD'),
-    'host': os.getenv('APP_HOST'),
-    'database': os.getenv('DB_DATABASE')
+    'host': os.getenv('APP_DB_HOST'),
+    'database': os.getenv('DB_DATABASE'),
+    'unix_socket': '/var/lib/mysql/mysql.sock'
 }
 connection = pymysql.connect(**config)
